e166038e50aa94ee38fe8a49290debf79eaf97cd
Andr-Shch <sensor121@gmail.com>
2021-11-02 18:24:55 +0200

    first commit

---
+++ 2-names.js
@@ -0,0 +1,6 @@
+const secret = 'sueper'
+//for shearing
+const peater = 'peater';
+const jhon = 'jhon';
+
+module.exports = {peater, jhon}
---
+++ 4-modules.js
@@ -0,0 +1,7 @@
+const names = require('./2-names')
+const sayHi = require('./utils')
+require('./3-exportFun')
+
+console.log(names);
+sayHi(names.peater);
+sayHi(names.jhon); 
